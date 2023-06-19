95db720d16aa5938e36b907b8b59cea34b77123a
João Lucas <joaolucaspinheirotorres@gmail.com>
2023-06-19 13:08:47 -0300

    Update

---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function Lista() {
+
+  const tarefas = [
+    {
+      tarefa: "React",
+      tempo: "01:00:00"
+    },
+    {
+      tarefa: "Typescript",
+      tempo: "02:00:00"
+    }
+  ];
+
+  return (
+    <aside>
+      <ul>
+        <h2>
+          Estudos do dia
+        </h2>
+        {tarefas.map((item, index) => (
+          <li key={index}>
+            <h3>{item.tarefa}</h3>
+            <span>{item.tempo}</span>
+          </li>
+        ))}
+      </ul>
+    </aside>
+
+  )
+}
